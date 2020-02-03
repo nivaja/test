515ce657f9d8b989f8b300d74374e800773b74b7
nivaja <nivajasingh359@gmail.com>
2020-02-03 12:27:43 +0545

    HEROKU TEST

--- MS_Transportation/settings.py
+++ MS_Transportation/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/static/',
+]
+
