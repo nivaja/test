ad3042d303e901bf50c10e1eaaeab72fae93f271
nivaja <nivajasingh359@gmail.com>
2020-02-03 13:30:12 +0545

    HEROKU TEST

--- MS_Transportation/settings.py
+++ MS_Transportation/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -125,4 +125,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     '/static/',
 ]
+django_heroku.settings(locals())
+
+
 
