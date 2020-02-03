997a7aed25e3e42dccaae4a244103cf2b9dbc47b
nivaja <nivajasingh359@gmail.com>
2020-02-03 14:58:52 +0545

    INITIAL COMMIT

--- MS_Transportation/settings.py
+++ MS_Transportation/settings.py
@@ -48,7 +48,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
 
 ROOT_URLCONF = 'MS_Transportation.urls'
 
